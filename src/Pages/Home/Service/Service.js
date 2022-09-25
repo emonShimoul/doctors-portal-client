d3a37cb72dc8289ae161d939a4462e045e23636c
emonShimoul <emonshimoul@gmail.com>
2022-09-25 22:29:24 +0600

    remove border and box shadow from Box

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -11,7 +11,7 @@ const Service = (props) => {
     const {name, description, img} = props.service;
     return (
         <Grid item xs={4} sm={4} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card sx={{ minWidth: 275, border: 0, boxShadow: 0 }}>
             <CardMedia
                 component="img"
                 style={{width: 'auto', height: '80px', margin:'0 auto'}}
@@ -19,19 +19,13 @@ const Service = (props) => {
                 alt="Paella dish"
             />
             <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-                </Typography>
                 <Typography variant="h5" component="div">
                 {name}
                 </Typography>
-                <Typography variant="body2">
+                <Typography variant="body2" color="text.secondary">
                 {description}
                 </Typography>
             </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
             </Card>
         </Grid>
     );
