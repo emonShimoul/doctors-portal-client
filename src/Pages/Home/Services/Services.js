8ef01ffa13977e134b838754572130a412437e90
emonShimoul <emonshimoul@gmail.com>
2022-09-25 22:39:35 +0600

    mui typography applied

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import Service from '../Service/Service';
 import fluoride from '../../../images/fluoride.png';
 import cavity from '../../../images/cavity.png';
@@ -31,6 +31,12 @@ const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
+                <Typography variant="h6" component="div" sx={{ fontWeight: 500, m: 2, color: 'success.main' }}>
+                    OUR SERVICES
+                </Typography>
+                <Typography variant="h4" component="div" sx={{ fontWeight: 600, m: 3 }}>
+                    Services We Provide
+                </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {services.map(service => <Service
                         key={service.name}
