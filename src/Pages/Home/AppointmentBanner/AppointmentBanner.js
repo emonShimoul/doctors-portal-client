303cda8b58fb2ca3d41eef8268a83061180dbc79
emonShimoul <emonshimoul@gmail.com>
2022-09-25 23:23:16 +0600

    added appointment banner bg image

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import doctor from '../../../images/doctor.png';
+import appointmentbg from '../../../images/appointment-bg.png';
 import { Typography } from '@mui/material';
+
+const appointmentBg = {
+    background: `url(${appointmentbg})`
+}
 
 const AppointmentBanner = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
                     <img 
