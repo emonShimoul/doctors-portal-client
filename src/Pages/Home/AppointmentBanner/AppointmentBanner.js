4dd45fac54c2d0a066e45bcb5e08c358130ca14c
emonShimoul <emonshimoul@gmail.com>
2022-09-25 23:30:37 +0600

    use background blend mode

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -5,17 +5,20 @@ import doctor from '../../../images/doctor.png';
 import appointmentbg from '../../../images/appointment-bg.png';
 import { Typography } from '@mui/material';
 
-const appointmentBg = {
-    background: `url(${appointmentbg})`
+const appointmentBanner = {
+    background: `url(${appointmentbg})`,
+    backgroundColor: 'rgba(45, 58, 74, 0.85)',
+    backgroundBlendMode: 'darken, luminosity',
+    marginTop: 100
 }
 
 const AppointmentBanner = () => {
     return (
-        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
                     <img 
-                    style={{width: 400, }}
+                    style={{width: 400, marginTop: '-115px'}}
                     src={doctor} alt="" />
                 </Grid>
                 <Grid item xs={12} md={6}>
