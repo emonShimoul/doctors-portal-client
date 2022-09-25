70659dcb6ce0808d9dadbe6ba375d222132a75aa
emonShimoul <emonshimoul@gmail.com>
2022-09-25 23:19:53 +0600

    use mui grid

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import doctor from '../../../images/doctor.png';
+import { Typography } from '@mui/material';
 
 const AppointmentBanner = () => {
     return (
-        <div>
-            <h2>This is Appointment</h2>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <img 
+                    style={{width: 400, }}
+                    src={doctor} alt="" />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Typography variant='h6'>
+                        Appointment
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
