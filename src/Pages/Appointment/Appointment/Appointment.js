b90c630c12d32714029f50e4da68ebc951bd2202
emonShimoul <emonshimoul@gmail.com>
2022-09-28 00:25:23 +0600

    slight changes

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
+import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
+import AvailableAppointment from '../AvailableAppointment/AvailableAppointment';
 
 const Appointment = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <AppointmentHeader></AppointmentHeader>
+            <AvailableAppointment></AvailableAppointment>
         </div>
     );
 };
