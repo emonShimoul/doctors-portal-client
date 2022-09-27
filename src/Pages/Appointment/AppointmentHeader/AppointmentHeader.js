32409affe1fd04bc41ef3f4c323ed83583d687b6
emonShimoul <emonshimoul@gmail.com>
2022-09-28 00:57:25 +0600

    use mui component

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,13 +1,14 @@
 import { Container, Grid } from '@mui/material';
 import React from 'react';
 import chair from '../../../images/chair.png';
+import Calendar from '../../Shared/Calendar/Calendar';
 
 const AppointmentHeader = () => {
     return (
         <Container>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                    ca
+                    <Calendar></Calendar>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <img style={{width:'100%'}} src={chair} alt="" />
--- src/Pages/Shared/Calendar/Calendar.js
+++ src/Pages/Shared/Calendar/Calendar.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
+const isWeekend = (date) => {
+    const day = date.day();
+  
+    return day === 0 || day === 6;
+  };
 
 const Calendar = () => {
+    const [value, setValue] = React.useState(dayjs());
+    
     return (
-        <div>
-            
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDatePicker
+                orientation="landscape"
+                openTo="day"
+                value={value}
+                shouldDisableDate={isWeekend}
+                onChange={(newValue) => {
+                setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
     );
 };
 
