0e03361f843bd5d3474a2ddf9bd2aedfe023f1be
emonShimoul <emonshimoul@gmail.com>
2022-09-28 00:20:43 +0600

    make available appointment component

---
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvailableAppointment = () => {
+    return (
+        <div>
+            <h2>available appointment</h2>
+        </div>
+    );
+};
+
+export default AvailableAppointment;
