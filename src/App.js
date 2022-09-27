44add7c14797197f76137cd9d16e063f9ff17f52
emonShimoul <emonshimoul@gmail.com>
2022-09-28 00:15:02 +0600

    make appointment component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
   Routes,
 } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
+import Appointment from './Pages/Appointment/Appointment/Appointment';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route element={<Appointment />} path="/appointment"></Route>
           <Route element={<Home />} path="/"></Route>
         </Routes>
       </BrowserRouter>
