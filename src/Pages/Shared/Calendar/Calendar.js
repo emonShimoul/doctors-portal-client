e5a57587bb5bdd33d1a385c1c486d5edb5e65e70
emonShimoul <emonshimoul@gmail.com>
2022-09-28 01:05:10 +0600

    use mui calendar

--- src/Pages/Shared/Calendar/Calendar.js
+++ src/Pages/Shared/Calendar/Calendar.js
@@ -16,17 +16,15 @@ const Calendar = () => {
     
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <StaticDatePicker
-                orientation="landscape"
-                openTo="day"
-                value={value}
-                shouldDisableDate={isWeekend}
-                onChange={(newValue) => {
-                setValue(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-            />
-        </LocalizationProvider>
+        <StaticDatePicker
+            displayStaticWrapperAs="desktop"
+            value={value}
+            onChange={(newValue) => {
+            setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+        />
+    </LocalizationProvider>
     );
 };
 
