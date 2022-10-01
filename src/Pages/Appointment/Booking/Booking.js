2c1b94823a7df86b9ca13225c5af54b8fb35e459
emonShimoul <emonshimoul@gmail.com>
2022-10-02 00:04:17 +0600

    used mui Paper and Typography

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,11 +1,17 @@
 import { Grid } from '@mui/material';
 import React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 const Booking = ({booking}) => {
     const {name, time, space} = booking;
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <h2>Booking</h2>    
+            <Paper elevation={3}>
+            <Typography variant="h5" gutterBottom>
+                {name}
+            </Typography>
+            </Paper>  
         </Grid>
     );
 };
