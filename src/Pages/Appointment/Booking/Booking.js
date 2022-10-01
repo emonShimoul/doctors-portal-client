73127c2a401002c5362e62e0aa698726b7d7b4d6
emonShimoul <emonshimoul@gmail.com>
2022-10-02 00:10:53 +0600

    added mui button

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -2,21 +2,23 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Booking = ({booking}) => {
     const {name, time, space} = booking;
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={3}>
-            <Typography variant="h5" gutterBottom>
-                {name}
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-                {time}
-            </Typography>
-            <Typography variant="caption" display="block" gutterBottom>
-                {space} SPACES AVAILABLE
-            </Typography>
+            <Paper elevation={3} sx={{py: 5}}>
+                <Typography sx={{color: 'info.main'}} variant="h5" gutterBottom>
+                    {name}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                    {time}
+                </Typography>
+                <Typography variant="caption" display="block" gutterBottom>
+                    {space} SPACES AVAILABLE
+                </Typography>
+                <Button variant="contained">BOOK APPOINTMENT</Button>
             </Paper>  
         </Grid>
     );
