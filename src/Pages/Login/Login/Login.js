782d5e2b4d57f4a37d81a91543691c7e2cd7cba0
emonShimoul <emonshimoul@gmail.com>
2022-11-11 02:38:43 +0600

    handle login success failure and set login page

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,55 +1,60 @@
-import { Container, Grid, Typography, TextField, Button } from '@mui/material';
+import { Container, Grid, Typography, TextField, Button, CircularProgress, Alert } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 import login from '../../../images/login.png'
 
 const Login = () => {
     const [loginData, setLoginData] = useState({});
+    const { user, loginUser, isLoading, authError } = useAuth();
     const handleOnChange = e => {
         const field = e.target.name;
         const value = e.target.value;
-         console.log(field, value);
-         const newLoginData = {...loginData};
-         newLoginData[field] = value;
-         setLoginData(newLoginData);
+        console.log(field, value);
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
     }
     const handleLoginSubmit = e => {
-        alert("Hello")
+        loginUser(loginData.email, loginData.password);
         e.preventDefault();
     }
     return (
         <Container>
             <Grid container spacing={2}>
-                <Grid item sx={{mt: 8}} xs={12} md={6}>
-                <Typography variant="body1" gutterBottom>
-                    Login
-                </Typography>
-                <form onSubmit={handleLoginSubmit}>
-                    <TextField
-                    sx={{width:'75%', m:1}}
-                        id="standard-basic"
-                        label="Your Email"
-                        name="email"
-                        onChange={handleOnChange}
-                        variant="standard" />
-                    <TextField
-                    sx={{width:'75%', m:1}}
-                        id="standard-basic"
-                        label="Your Password"
-                        type="password"
-                        name="password"
-                        onChange={handleOnChange}
-                        variant="standard" />
-                        <Button sx={{width:'75%', m:1}} type="submit" variant="contained">Login</Button>
+                <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>
+                        Login
+                    </Typography>
+                    <form onSubmit={handleLoginSubmit}>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Password"
+                            type="password"
+                            name="password"
+                            onChange={handleOnChange}
+                            variant="standard" />
+                        <Button sx={{ width: '75%', m: 1 }} type="submit" variant="contained">Login</Button>
                         <NavLink
-                        style={{textDecoration: 'none'}}
-                        to="/register"><Button variant="text">New User?? Please Register</Button>
+                            style={{ textDecoration: 'none' }}
+                            to="/register"><Button variant="text">New User?? Please Register</Button>
                         </NavLink>
-                </form>
+                        {isLoading && <CircularProgress />}
+                        {user?.email && <Alert severity="success">User Login Successfully!!</Alert>}
+                        {authError && <Alert severity="error">{authError}</Alert>}
+                    </form>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                    <img style={{width:'100%'}} src={login} alt="" />
+                    <img style={{ width: '100%' }} src={login} alt="" />
                 </Grid>
             </Grid>
         </Container>
