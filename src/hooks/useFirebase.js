6d3c170bc2218b5b1c6df87a5d5c3b4a46bf81c6
emonShimoul <emonshimoul@gmail.com>
2022-11-11 02:14:01 +0600

    set loading option for register or login

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -8,57 +8,65 @@ initializeFirebase();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const auth = getAuth();
 
     const registerUser = (email, password) => {
+        setIsLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-          });
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            })
+            .finally(() => setIsLoading(false));
     }
 
     const loginUser = (email, password) => {
+        setIsLoading(true);
         signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            })
+            .finally(() => setIsLoading(false));
     }
 
     // observe user state
     useEffect(() => {
         const unsubscribed = onAuthStateChanged(auth, (user) => {
             if (user) {
-              setUser(user);
+                setUser(user);
             } else {
-              setUser({})
+                setUser({})
             }
-          });
-          return () => unsubscribed;
+            setIsLoading(false);
+        });
+        return () => unsubscribed;
     }, [])
 
     const logout = () => {
         signOut(auth).then(() => {
             // Sign-out successful.
-          }).catch((error) => {
+        }).catch((error) => {
             // An error happened.
-          });
+        })
+            .finally(() => setIsLoading(false));
     }
 
     return {
         user,
+        isLoading,
         registerUser,
         loginUser,
         logout
