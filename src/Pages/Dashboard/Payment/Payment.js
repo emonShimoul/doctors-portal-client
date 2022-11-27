e656d912a5e6c8d95f4562de5175d46068c7119b
emonShimoul <emonshimoul@gmail.com>
2022-11-28 00:56:00 +0600

    display appointment price

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Payment = () => {
     const { appointmentId } = useParams();
+    const [appointment, setAppointment] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/appointments/${appointmentId}`)
+            .then(res => res.json())
+            .then(data => setAppointment(data))
+    }, [appointmentId])
     return (
         <div>
             <h2>Please Pay for: {appointmentId}</h2>
+            <h4>Pay: ${appointment.price}</h4>
         </div>
     );
 };
