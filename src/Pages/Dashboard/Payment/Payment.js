4ef099a427ccf3f45570551c5c5a8e916d82018e
emonShimoul <emonshimoul@gmail.com>
2022-11-29 22:14:20 +0600

    added credit card options

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51M8NxHKJLu5mojoq47wxLpr8vuVuxC3ciSUVdD6r5l1FYADJzuB2NETcMCeotP55f3Xc9XvsxoqkxjIlU4NvNCJz00CJ8vgbVJ')
 
 const Payment = () => {
     const { appointmentId } = useParams();
@@ -13,6 +18,9 @@ const Payment = () => {
         <div>
             <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
             <h4>Pay: ${appointment.price}</h4>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
