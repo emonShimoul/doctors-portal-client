7d7810c171290b453cb9f56cae7161c7813ee99a
emonShimoul <emonshimoul@gmail.com>
2022-11-30 23:15:41 +0600

    remove unnecessary code

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -39,7 +39,6 @@ const useFirebase = () => {
             })
             .catch((error) => {
                 setAuthError(error.message);
-                // ..
             })
             .finally(() => setIsLoading(false));
     }
