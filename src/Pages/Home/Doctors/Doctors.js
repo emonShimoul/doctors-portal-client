592cb6e20b2c6c81a5f9750cc0d9851398f68486
emonShimoul <emonshimoul@gmail.com>
2022-12-10 19:10:07 +0600

    display the number of total doctors

--- src/Pages/Home/Doctors/Doctors.js
+++ src/Pages/Home/Doctors/Doctors.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Doctors = () => {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/doctors')
+            .then(res => res.json())
+            .then(data => setDoctors(data))
+    }, [])
     return (
         <div>
-            <h2>Our Doctors</h2>
+            <h2>Our Doctors: {doctors.length}</h2>
         </div>
     );
 };
